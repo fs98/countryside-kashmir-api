b2a894fbc94a7732a9d4a8a4df18bd37575cee56
Fata Sefer <sefer.fata@gmail.com>
2022-08-19 18:18:54 +0200

    feat: create message model

---
+++ app/Models/Message.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use function PHPSTORM_META\map;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email',
+        'content',
+        'user_id'
+    ];
+}
