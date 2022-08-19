8e947378cf8f09d3e3e465850e06e010d08c8531
Fata Sefer <sefer.fata@gmail.com>
2022-08-19 17:47:07 +0200

    feat: register category api resource routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +23,9 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResources([
         'authors' => AuthorController::class,
+    ]);
+
+    Route::apiResources([
+        'categories' => CategoryController::class,
     ]);
 });
