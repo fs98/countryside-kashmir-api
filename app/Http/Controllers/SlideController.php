ba8101d6da7f367681626f4fe93872da559c66a3
Fata Sefer <sefer.fata@gmail.com>
2022-08-20 20:48:52 +0200

    refactor: improve logic and shorten the update function

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -75,10 +75,8 @@ class SlideController extends BaseController
      */
     public function update(UpdateSlideRequest $request, Slide $slide)
     {
-        // Probably needs refactoring
-        $request->order ? $slide->order = $request->order : $slide->order;
-        $request->title ? $slide->title = $request->title : $slide->title;
-        $request->subtitle ? $slide->subtitle = $request->subtitle : $slide->subtitle;
+        // So we can override the request image value
+        $requestData = $request->all();
 
         if ($request->hasFile('image')) {
 
@@ -87,12 +85,12 @@ class SlideController extends BaseController
 
             // Save new photo
             $path = Storage::disk('public')->putFile('slides', $request->file('image'));
-            $slide->image = $path;
+
+            // Override image value
+            $requestData['image'] = $path;
         }
 
-        $slide->update();
-
-        if ($slide) {
+        if ($slide->update($requestData)) {
             return $this->sendResponse($slide, 'Slide successfully updated!');
         }
 
