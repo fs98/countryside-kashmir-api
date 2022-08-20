d61dcfdd95436b4528a3f5f1ffc58b04b834caa7
Fata Sefer <sefer.fata@gmail.com>
2022-08-20 10:09:32 +0200

    feat: create slides migration

---
+++ database/migrations/2022_08_20_100556_create_slides_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->id();
+            $table->string('url', 128);
+            $table->integer('order')->nullable();
+            $table->string('title', 64)->nullable();
+            $table->string('subtitle', 32)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+};
