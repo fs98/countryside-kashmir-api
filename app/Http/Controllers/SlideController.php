65c28c0324f10816333a4e42a0393dd769550527
Fata Sefer <sefer.fata@gmail.com>
2022-08-20 10:48:05 +0200

    feat: write slide controller index function

---
+++ app/Http/Controllers/SlideController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSlideRequest;
+use App\Http\Requests\UpdateSlideRequest;
+use App\Http\Resources\SlideResource;
+use App\Models\Slide;
+
+class SlideController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $slides = Slide::all();
+        return SlideResource::collection($slides);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSlideRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSlideRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Slide  $slide
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Slide $slide)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSlideRequest  $request
+     * @param  \App\Models\Slide  $slide
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSlideRequest $request, Slide $slide)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Slide  $slide
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Slide $slide)
+    {
+        //
+    }
+}
