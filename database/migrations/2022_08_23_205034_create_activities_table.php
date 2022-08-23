7f3cfd8a4ebaa6a1b0b60fa4d93af5355aa21724
Fata Sefer <sefer.fata@gmail.com>
2022-08-23 20:52:23 +0200

    feat: create activities migration

---
+++ database/migrations/2022_08_23_205034_create_activities_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->json('description');
+            $table->string('image', 128);
+            $table->string('image_alt', 64);
+            $table->string('keywords', 255);
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('author_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+};
