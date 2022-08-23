c3829bf4e870ba4763ffc916e51b92c86b511cbc
Fata Sefer <sefer.fata@gmail.com>
2022-08-23 21:26:06 +0200

    feat: create activity image factory

---
+++ database/factories/ActivityImageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Activity;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ActivityImage>
+ */
+class ActivityImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'image' => fake()->imageUrl(),
+            'image_alt' => fake()->sentence($nbWords = 4, $variableNbWords = true),
+            'activity_id' => Activity::pluck('id')->random(),
+            'user_id' => User::role(['Super Admin', 'Admin'])->pluck('id')->random(),
+        ];
+    }
+}
