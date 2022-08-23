da1867f228bcfd9df7c9239a31e3ff500f7e0f01
Fata Sefer <sefer.fata@gmail.com>
2022-08-23 21:31:52 +0200

    feat: authorize activity image api resource

--- app/Http/Controllers/ActivityImageController.php
+++ app/Http/Controllers/ActivityImageController.php
@@ -9,6 +9,16 @@ use App\Models\ActivityImage;
 class ActivityImageController extends Controller
 {
     /**
+     * Create the controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(ActivityImage::class, 'activityImage');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
