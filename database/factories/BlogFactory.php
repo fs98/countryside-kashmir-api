9e3e5d6054b0cfa485505f2f9a9a57ed84dedd8f
Fata Sefer <sefer.fata@gmail.com>
2022-08-24 23:00:55 +0200

    feat: create blog factory

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => ucfirst(fake()->word()),
+            'content' => json_encode(fake()->paragraphs(3, false)),
+            'image' => fake()->imageUrl(),
+            'image_alt' => fake()->sentence($nbWords = 6, $variableNbWords = true),
+            'keywords' => fake()->words($nb = 3, $asText = true),
+            'user_id' => User::role(['Super Admin', 'Admin'])->pluck('id')->random(),
+            'author_id' => Author::pluck('id')->random(),
+            'published_at' => fake()->dateTimeThisMonth(),
+        ];
+    }
+}
