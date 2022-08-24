9f3d9e2be7fa2dd361fb16f9a673efa263d1f569
Fata Sefer <sefer.fata@gmail.com>
2022-08-24 22:51:15 +0200

    feat: create blog model

---
+++ app/Models/Blog.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'image_alt',
+        'keywords',
+        'user_id',
+        'author_id',
+        'published_at'
+    ];
+}
