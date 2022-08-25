52a29746b17578552ed095773b45e3f99e83ddf3
Fata Sefer <sefer.fata@gmail.com>
2022-08-25 17:57:18 +0200

    feat: define update package image request validation rules

--- app/Http/Requests/UpdatePackageImageRequest.php
+++ app/Http/Requests/UpdatePackageImageRequest.php
@@ -13,7 +13,7 @@ class UpdatePackageImageRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class UpdatePackageImageRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'image' => 'image|mimes:jpg,png,jpeg|max:5000',
+            'image_alt' => 'string|max:64',
         ];
     }
 }
