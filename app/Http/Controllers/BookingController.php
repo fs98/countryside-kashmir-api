8392bccf0428cd24388be97934607f6c6607ee9a
Fata Sefer <sefer.fata@gmail.com>
2022-08-25 18:21:41 +0200

    feat: write booking index function

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookingRequest;
+use App\Http\Requests\UpdateBookingRequest;
+use App\Http\Resources\BookingResource;
+use App\Models\Booking;
+
+class BookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = auth()->user();
+
+        if ($user->hasRole('Client')) {
+            $bookings = $user->bookings()->get();
+        } else {
+            $bookings = Booking::with('user')->get();
+        }
+
+        return BookingResource::collection($bookings);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBookingRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBookingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBookingRequest  $request
+     * @param  \App\Models\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBookingRequest $request, Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Booking $booking)
+    {
+        //
+    }
+}
