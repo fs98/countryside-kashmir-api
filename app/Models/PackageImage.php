8ffbfb601bff231c39a0eee0901b3f49f56932a0
Fata Sefer <sefer.fata@gmail.com>
2022-08-25 17:33:16 +0200

    feat: make package image model

---
+++ app/Models/PackageImage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PackageImage extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'image',
+        'image_alt',
+        'package_id',
+        'user_id',
+    ];
+}
