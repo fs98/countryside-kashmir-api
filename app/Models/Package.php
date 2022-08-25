acc5512a7edc4338d03ed4ab6884c021da606e2b
Fata Sefer <sefer.fata@gmail.com>
2022-08-25 16:13:54 +0200

    feat: create package description accessor to json decode it

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -50,5 +51,17 @@ class Package extends Model
     public function author()
     {
         return $this->belongsTo(Author::class);
+    }
+
+    /**
+     * Get the destinations's description.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function description(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => json_decode($value),
+        );
     }
 }
