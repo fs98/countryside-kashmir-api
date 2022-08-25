0a96ae50a3dedf9ff48516aabd355b603243861e
Fata Sefer <sefer.fata@gmail.com>
2022-08-25 19:56:05 +0200

    feat: make user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -16,17 +17,29 @@ class UserSeeder extends Seeder
     public function run()
     {
         $superadmin = User::factory()->create([
+            'name' => 'Super Admin',
             'email' => 'superadmin@countrysidekashmir.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ]);
         $superadmin->assignRole('Super Admin');
 
         $superadmin = User::factory()->create([
+            'name' => 'Admin',
             'email' => 'admin@countrysidekashmir.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ]);
         $superadmin->assignRole('Admin');
 
         $superadmin = User::factory()->create([
+            'name' => 'Client',
             'email' => 'client@countrysidekashmir.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ]);
         $superadmin->assignRole('Client');
     }
