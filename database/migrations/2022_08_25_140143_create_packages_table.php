21b5c926bb0a8ea40a027a9992f36f1246c99ced
Fata Sefer <sefer.fata@gmail.com>
2022-08-25 15:35:32 +0200

    feat: create packages migration

---
+++ database/migrations/2022_08_25_140143_create_packages_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->json('description');
+            $table->string('image', 128);
+            $table->string('image_alt', 64);
+            $table->smallInteger('duration', false, true);
+            $table->smallInteger('price', false, true);
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->tinyInteger('persons', false, true);
+            $table->string('keywords', 255);
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('author_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+};
