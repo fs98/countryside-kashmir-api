789760f6cf3d585cc01bf8330bf6174912e3189f
Fata Sefer <sefer.fata@gmail.com>
2022-08-25 17:35:42 +0200

    feat: create package images migration

---
+++ database/migrations/2022_08_25_173234_create_package_images_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('package_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image', 128);
+            $table->string('image_alt', 64);
+            $table->foreignId('package_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('package_images');
+    }
+};
