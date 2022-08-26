e071ed608a30c477638d54dae47870dab13bb2dc
Fata Sefer <sefer.fata@gmail.com>
2022-08-26 22:17:43 +0200

    feat: add slug to category model

--- database/migrations/2022_08_19_172237_create_categories_table.php
+++ database/migrations/2022_08_19_172237_create_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 32);
+            $table->string('slug', 64);
             $table->timestamps();
         });
     }
