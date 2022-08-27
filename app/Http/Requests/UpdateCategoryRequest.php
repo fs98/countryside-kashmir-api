469008be73c4393f174262ac265cd386d0786ace
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 20:57:00 +0200

    feat: update update category request validation rules

--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -25,7 +25,6 @@ class UpdateCategoryRequest extends FormRequest
     {
         return [
             'name' => [
-                'required',
                 'string',
                 'unique:categories',
                 'max:32'
