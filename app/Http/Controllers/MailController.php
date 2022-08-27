42a546be5aa686cff4ec8bcade42c52e811360f0
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 23:22:57 +0200

    feat: write sendMail function

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\MailRequest;
+use App\Mail\MailToClient;
+use Illuminate\Support\Facades\Mail;
 
-class MailController extends Controller
+class MailController extends BaseController
 {
     /**
      * Send a mail.
      *
      * @return \Illuminate\Http\Response
      */
-    public function sendMail()
+    public function sendMail(MailRequest $request)
     {
+        $mail = Mail::to($request->receiver)
+            ->send(new MailToClient($request->subject, $request->message));
+
+        if ($mail) {
+            return $this->sendResponse(null, 'Email successfully sent!');
+        }
+
+        return $this->sendError(null, 'Email sending failed!');
     }
 }
