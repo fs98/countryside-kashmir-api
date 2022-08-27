1ef2e8bd5aa47521e059cbd85a83673786648307
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 21:17:30 +0200

    feat: format bookings migration

--- database/migrations/2022_08_25_180220_create_bookings_table.php
+++ database/migrations/2022_08_25_180220_create_bookings_table.php
@@ -15,23 +15,18 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
-
             $table->string('name', 64);
             $table->string('email', 128)->nullable();
             $table->string('phone_number', 128);
             $table->string('address', 128);
             $table->string('city', 128);
             $table->string('country', 128);
-
             $table->smallInteger('persons', false, true);
             $table->smallInteger('adults', false, true);
             $table->smallInteger('children', false, true);
-
             $table->timestamp('arrival_date');
-
             $table->smallInteger('days', false, true);
             $table->smallInteger('nights', false, true);
-
             $table->foreignId('package_id')
                 ->nullable()
                 ->constrained()
@@ -42,7 +37,6 @@ return new class extends Migration
                 ->constrained()
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
-
             $table->timestamps();
         });
     }
