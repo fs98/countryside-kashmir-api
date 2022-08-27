ac2065226ae9f2ca6e9860d8260607003ac8aeb1
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 14:15:01 +0200

    feat: set slug when setting name in destination model

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Destination extends Model
 {
@@ -99,6 +100,22 @@ class Destination extends Model
     {
         return Attribute::make(
             get: fn () => asset('storage/' . $this->image),
+        );
+    }
+
+    /**
+     * Interact with the blog's name and slug.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value,
+            set: fn ($value) => [
+                'name' => $value,
+                'slug' => Str::slug($value),
+            ],
         );
     }
 }
