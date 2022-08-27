6d33a4fbcb17941c6986312f16cb49acfb733de0
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 21:14:50 +0200

    feat: set blogs title to be unique

--- database/migrations/2022_08_24_224848_create_blogs_table.php
+++ database/migrations/2022_08_24_224848_create_blogs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 64);
+            $table->string('title', 64)->unique();
             $table->string('slug', 128);
             $table->json('content');
             $table->string('image', 128);
