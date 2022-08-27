fd792c78194ad6fad14d73a24c23b47e47c05fde
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 23:13:38 +0200

    feat: define mail request validation rules

---
+++ app/Http/Requests/MailRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->hasRole('Admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'receiver' => [
+                'required',
+                'email'
+            ],
+            'subject' => [
+                'required'
+            ],
+            'message' => [
+                'required'
+            ]
+        ];
+    }
+}
