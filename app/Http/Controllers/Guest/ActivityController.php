d27fc9e5edc903b4865cde4b43191fe59cb60e36
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 22:13:24 +0200

    feat: write guest activity controller index function

--- app/Http/Controllers/Guest/ActivityController.php
+++ app/Http/Controllers/Guest/ActivityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ActivityResource;
 use App\Models\Activity;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,8 @@ class ActivityController extends Controller
      */
     public function index()
     {
-        //
+        $activities = Activity::all();
+        return ActivityResource::collection($activities);
     }
 
     /**
