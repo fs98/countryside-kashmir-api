4a8ef932cb5fd9b83504cbf96cb689a70fd8fd13
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 22:31:00 +0200

    feat: create mail controller

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    /**
+     * Send a mail.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendMail()
+    {
+    }
+}
