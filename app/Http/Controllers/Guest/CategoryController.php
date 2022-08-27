1b15106c6df5f7538121a92bfcae82c649e64a3a
Fata Sefer <sefer.fata@gmail.com>
2022-08-27 22:02:22 +0200

    feat: write guest category controller show function

--- app/Http/Controllers/Guest/CategoryController.php
+++ app/Http/Controllers/Guest/CategoryController.php
@@ -39,7 +39,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return new CategoryResource($category);
     }
 
     /**
