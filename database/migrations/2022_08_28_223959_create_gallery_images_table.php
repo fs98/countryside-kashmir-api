1c08ca621ef2596a4a2da9150dc4f1561ecf16eb
Fata Sefer <sefer.fata@gmail.com>
2022-08-30 15:05:52 +0200

    feat: add deleted_at field to gallery images migration

--- database/migrations/2022_08_28_223959_create_gallery_images_table.php
+++ database/migrations/2022_08_28_223959_create_gallery_images_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
                 ->constrained()
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
