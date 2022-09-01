0e4050f5932deee03d07cbda88b5d6536fd62a06
Fata Sefer <sefer.fata@gmail.com>
2022-09-01 22:25:39 +0200

    feat: specifiy paramether binding for gallery_image to get even trashed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\GalleryImage;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -35,6 +36,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+
+        Route::bind('gallery_image', function ($id) {
+            return GalleryImage::withTrashed()->find($id);
         });
     }
 
