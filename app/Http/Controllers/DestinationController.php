7323ba02c1865e1fa5c9b2f7e791dfe7d8ea4800
Fata Sefer <sefer.fata@gmail.com>
2022-09-01 23:27:31 +0200

    refactor: replace updateImage with uploadImage  method in destination controller

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -77,7 +77,7 @@ class DestinationController extends BaseController
      */
     public function update(UpdateDestinationRequest $request, Destination $destination)
     {
-        $requestData = $this->updateImage($request, $destination->image, 'destinations');
+        $requestData = $this->uploadImage($request, 'destinations', $destination->image);
 
         /** 
          * Define the type of requestData to avoid error
