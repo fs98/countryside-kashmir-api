1fb4625702973a90f461c0a0b887eee9e8f3092a
Fata Sefer <sefer.fata@gmail.com>
2022-09-03 12:24:15 +0200

    refactor: remove \App\Models\DestinationImage imports in destination image factory

--- database/factories/DestinationImageFactory.php
+++ database/factories/DestinationImageFactory.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\DestinationImage>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
  */
 class DestinationImageFactory extends Factory
 {
