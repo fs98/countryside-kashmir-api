60327979502d981f8bc796ad6aad3647d5145be1
Fata Sefer <sefer.fata@gmail.com>
2022-09-03 10:42:04 +0200

    feat: create images migration

---
+++ database/migrations/2022_09_03_103454_create_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image', 128);
+            $table->string('image_alt', 64);
+            $table->integer('imageable_id');
+            $table->string('imageable_type');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
