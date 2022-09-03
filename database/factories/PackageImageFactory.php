3f5fd1ac6c37c5b1b1fc30705d356f7c0a1d85a1
Fata Sefer <sefer.fata@gmail.com>
2022-09-03 12:28:39 +0200

    refactor: remove \App\Models\PackageImage imports in package image factory

--- database/factories/PackageImageFactory.php
+++ database/factories/PackageImageFactory.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PackageImage>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
  */
 class PackageImageFactory extends Factory
 {
