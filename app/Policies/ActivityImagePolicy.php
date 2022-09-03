85388ae87b435faf2332135811001faff28e878a
Fata Sefer <sefer.fata@gmail.com>
2022-09-03 12:09:17 +0200

    refactor: remove \App\Models\ActivityImage imports in activity image policy

--- app/Policies/ActivityImagePolicy.php
+++ app/Policies/ActivityImagePolicy.php
@@ -2,7 +2,7 @@
 
 namespace App\Policies;
 
-use App\Models\ActivityImage;
+use App\Models\Image as ActivityImage;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
@@ -25,7 +25,7 @@ class ActivityImagePolicy
      * Determine whether the user can view the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\ActivityImage  $activityImage
+     * @param  \App\Models\Image  $activityImage
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function view(User $user, ActivityImage $activityImage)
@@ -48,7 +48,7 @@ class ActivityImagePolicy
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\ActivityImage  $activityImage
+     * @param  \App\Models\Image  $activityImage
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function update(User $user, ActivityImage $activityImage)
@@ -60,7 +60,7 @@ class ActivityImagePolicy
      * Determine whether the user can delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\ActivityImage  $activityImage
+     * @param  \App\Models\Image  $activityImage
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function delete(User $user, ActivityImage $activityImage)
@@ -72,7 +72,7 @@ class ActivityImagePolicy
      * Determine whether the user can restore the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\ActivityImage  $activityImage
+     * @param  \App\Models\Image  $activityImage
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function restore(User $user, ActivityImage $activityImage)
@@ -84,7 +84,7 @@ class ActivityImagePolicy
      * Determine whether the user can permanently delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\ActivityImage  $activityImage
+     * @param  \App\Models\Image  $activityImage
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function forceDelete(User $user, ActivityImage $activityImage)
