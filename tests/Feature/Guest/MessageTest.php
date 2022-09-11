1e04c0a4d01ddef107343c456a162e61c09ebe31
Fata Sefer <sefer.fata@gmail.com>
2022-09-11 12:53:35 +0200

    test: write `guest_can_send_a_message` test

---
+++ tests/Feature/Guest/MessageTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Guest;
+
+use App\Models\Message;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MessageTest extends TestCase
+{
+    /**
+     * Unauthenticated user can send a message.
+     *
+     * @return void
+     */
+    public function test_guest_can_send_a_message()
+    {
+        $response = $this->post('/api/guest/messages', [
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'phone_number' => fake()->phoneNumber(),
+            'email' => fake()->email(),
+            'content' => fake()->text(100),
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'success',
+                'data' => [
+                    'first_name',
+                    'last_name',
+                    'phone_number',
+                    'email',
+                    'content',
+                    'updated_at',
+                    'created_at',
+                    'id'
+                ],
+                'message',
+            ]);
+    }
+}
