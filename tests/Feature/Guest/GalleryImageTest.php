9f6769983c0a67aa9a8b3e12e7e4076132abd317
Fata Sefer <sefer.fata@gmail.com>
2022-09-11 13:25:20 +0200

    test: write `gallery_images_can_be_retrieved` test

---
+++ tests/Feature/Guest/GalleryImageTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature\Guest;
+
+use App\Models\GalleryImage;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GalleryImageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test index function.
+     *
+     * @return void
+     */
+    public function test_gallery_images_can_be_retrieved()
+    {
+        $user = User::factory()->create();
+
+        GalleryImage::create([
+            'image' => fake()->imageUrl(),
+            'image_alt' => fake()->sentence($nbWords = 3, $variableNbWords = true),
+            'user_id' => $user->id
+        ]);
+
+        $response = $this->get('/api/guest/gallery-images');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        [
+                            'id',
+                            'image_alt',
+                            'user_id',
+                            'deleted_at',
+                            'created_at',
+                            'updated_at',
+                            'image_url'
+                        ]
+                    ],
+                ]
+            );
+    }
+}
