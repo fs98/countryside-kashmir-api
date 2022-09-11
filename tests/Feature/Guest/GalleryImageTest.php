86906d6faecee7b3aac2a713b3faba373c0ef4bb
Fata Sefer <sefer.fata@gmail.com>
2022-09-11 13:27:27 +0200

    test: write `gallery_image_can_be_retrieved_by_id` test

--- tests/Feature/Guest/GalleryImageTest.php
+++ tests/Feature/Guest/GalleryImageTest.php
@@ -46,4 +46,37 @@ class GalleryImageTest extends TestCase
                 ]
             );
     }
+
+    /**
+     * Test show function.
+     *
+     * @return void
+     */
+    public function test_gallery_image_can_be_retrieved_by_id()
+    {
+        $user = User::factory()->create();
+
+        $galleryImage = GalleryImage::create([
+            'image' => fake()->imageUrl(),
+            'image_alt' => fake()->sentence($nbWords = 3, $variableNbWords = true),
+            'user_id' => $user->id
+        ]);
+
+        $response = $this->get('/api/guest/gallery-images/' . $galleryImage->id);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        'id',
+                        'image_alt',
+                        'user_id',
+                        'deleted_at',
+                        'created_at',
+                        'updated_at',
+                        'image_url'
+                    ]
+                ]
+            );
+    }
 }
