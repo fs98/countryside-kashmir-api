12bef1b64073e0cd52a18d5d85fe857a32796298
Fata Sefer <sefer.fata@gmail.com>
2022-09-18 23:43:29 +0200

    feat: write destinations_can_be_retrieved_for_guest_user test

---
+++ tests/Feature/Guest/DestinationTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Guest;
+
+use App\Models\Author;
+use App\Models\Destination;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Spatie\Permission\Models\Role;
+use Tests\TestCase;
+
+class DestinationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test index function.
+     *
+     * @return void
+     */
+    public function test_destinations_can_be_retrieved_for_guest_user()
+    {
+        $admin = Role::create(['name' => 'Admin']);
+        $superAdmin = Role::create(['name' => 'Super Admin']);
+
+        Destination::factory(5)
+            ->for(User::factory()->hasAttached($superAdmin)->create())
+            ->for(Author::factory()->create())
+            ->create();
+
+        $response = $this->get('/api/guest/destinations');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        [
+                            'name',
+                            'slug',
+                            'image_alt',
+                            'image_url'
+                        ]
+                    ]
+                ]
+            );
+    }
+}
