5607a1e534b5e37213e72c9bd771130060d4d82f
Fata Sefer <sefer.fata@gmail.com>
2022-10-01 22:56:05 +0200

    feat: remove pagination in author index function

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -26,7 +26,7 @@ class AuthorController extends BaseController
      */
     public function index()
     {
-        $authors = Author::paginate(10);
+        $authors = Author::get();
         return AuthorResource::collection($authors);
     }
 
